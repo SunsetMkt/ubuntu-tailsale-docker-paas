79f6f3d560735403335343e63adf4588f4812ad9
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-09-19 10:59:46 +0800

    An attempt to logout on exit

--- configure-ssh-user.sh
+++ configure-ssh-user.sh
@@ -32,7 +32,6 @@ fi
 
 # Setup Tailscale
 # https://tailscale.com/kb/1107/heroku
-# tailscale status --peers=false --json | grep -q 'Online.*true' # https://github.com/tailscale/tailscale/issues/12758
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --state=mem: &
 tailscale up --authkey="${TS_AUTHKEY}?preauthorized=true&ephemeral=true" --hostname=${TS_HOSTNAME} --advertise-exit-node=true --ssh=true --accept-dns --advertise-tags=tag:ci
 tailscale set --webclient=true
@@ -45,6 +44,14 @@ unset TS_AUTHKEY
 unset SSH_USERNAME
 unset PASSWORD
 unset AUTHORIZED_KEYS
+
+# An attempt to logout on exit
+cleanup() {
+    echo "Got SIGTERM..."
+    tailscale logout
+    exit 0
+}
+trap cleanup SIGTERM
 
 # Start the SSH server
 exec /usr/sbin/sshd -D
